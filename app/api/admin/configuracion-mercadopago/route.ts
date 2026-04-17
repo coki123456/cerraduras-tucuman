e9cf0e3a66f218ae9c3d51a0735d715d6a3cc46b
coki123456 <esteban_coki@hotmail.com>
2026-04-17 10:56:10 -0300

    Ver. 1.7.1

--- app/api/admin/configuracion-mercadopago/route.ts
+++ app/api/admin/configuracion-mercadopago/route.ts
@@ -38,7 +38,7 @@ export async function POST(req: NextRequest) {
     const validationResult = schemaMercadopagoConfig.safeParse(body);
     if (!validationResult.success) {
       return NextResponse.json(
-        { error: validationResult.error.errors[0].message },
+        { error: validationResult.error.issues[0].message },
         { status: 400 }
       );
     }
